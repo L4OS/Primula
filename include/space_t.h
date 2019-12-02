14f5e43eb1621b16027f822744d114104f31db7f
Aleksey Mandrykin <almandrykin@ya.ru>
2019-12-02 06:25:37 +0300

    space_t.h optimization

--- include/space_t.h
+++ include/space_t.h
@@ -2,15 +2,7 @@
 
 #include "statement_t.h"
 
-struct space_code_t
-{
-    std::list<type_t*>                  space_types_list;
-    std::list<variable_base_t*>         space_variables_list;
-    std::list<statement_t *>            space_code;
-    std::list<class function_parser*>   function_list;
-};
-
-struct space_t : public space_code_t
+struct space_t
 {
     typedef enum
     {
@@ -25,12 +17,15 @@ struct space_t : public space_code_t
         template_space
     } spacetype_t;
 
-    std::map<std::string, type_t*>                  space_types_map;
-    std::map<std::string, variable_base_t*>         space_variables_map;
-    std::map<std::string, class function_parser*>   function_map;
-    std::map<std::string, int>                      enum_map;
+    typedef std::list<structure_t*>		parent_spaces_t;
 
-    spacetype_t		type;
-    namespace_t	*	parent;
-    std::string		name;
+    spacetype_t		                    type;
+    std::string		                    name;
+    namespace_t                     *   parent;
+    parent_spaces_t						inherited_from;
+
+    std::list<type_t*>                  space_types_list;
+    std::list<variable_base_t*>         space_variables_list;
+    std::list<statement_t *>            space_code;
+    std::list<class function_parser*>   function_list;
 };
